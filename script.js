eebe4e7b6d40a10830cac4338b2a2c027d098ff1
camposbryan2011-dotcom <camposbryan.2011@gmail.com>
2026-04-23 18:01:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// pega o visor (input)
+const visor = document.querySelector("#visor");
 
+// pega todos os botões
+const botoes = document.querySelectorAll("button");
+
+// percorre cada botão
+botoes.forEach(function(botao) {
+
+    // quando clicar em um botão
+    botao.addEventListener("click", function() {
+
+        const valor = botao.textContent;
+
+        // se clicar no C → limpa tudo
+        if (valor === "C") {
+            visor.value = "";
+
+        // se clicar no = → calcula
+        } else if (valor === "=") {
+            visor.value = eval(visor.value);
+
+        // qualquer outro botão → adiciona no visor
+        } else {
+            visor.value += valor;
+        }
+
+    });
+
+});
